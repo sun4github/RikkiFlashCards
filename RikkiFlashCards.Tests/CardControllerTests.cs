457922bcb5aa007f9cbba2925e37b1fd5ef94802
Suneel Kanuri <suneel.kanuri@gmail.com>
2020-07-06 22:30:03 -0400

    - CardControllerTests WIP

--- RikkiFlashCards.Tests/CardControllerTests.cs
+++ RikkiFlashCards.Tests/CardControllerTests.cs
@@ -1,18 +1,21 @@
+using AnkiFlashCards.Controllers;
 using AnkiFlashCards.Data;
+using AnkiFlashCards.Models.Domain;
 using AnkiFlashCards.Models.DTO;
 using AnkiFlashCards.Services.Contracts;
 using Moq;
 using RikkiFlashCards.Services.Contracts;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace RikkiFlashCards.Tests
 {
     public class CardControllerTests
     {
-        private IDeckService _deckService;
-        private IRepositoryWrapper _repositoryWrapper;
-        private ICodeRenderService _codeRenderService;
+        private Mock<IDeckService> _deckService;
+        private Mock<IRepositoryWrapper> _repositoryWrapper;
+        private Mock<ICodeRenderService> _codeRenderService;
 
         public CardControllerTests()
         {
@@ -21,29 +24,77 @@ namespace RikkiFlashCards.Tests
 
         private void SetupMocks()
         {
-            var deckService = new Mock<IDeckService>();
-            var repositoryWrapper = new Mock<IRepositoryWrapper>();
-            var codeRenderService = new Mock<ICodeRenderService>();
+            _deckService = new Mock<IDeckService>();
+            _repositoryWrapper = new Mock<IRepositoryWrapper>();
+            _codeRenderService = new Mock<ICodeRenderService>();
 
-            deckService
+            var mockCardListDto = new CardListDto
+            {
+                CardCount = 2,
+                Cards = new List<Card> {
+                        new Card
+                        {
+                            CardId = 1,
+                            Front = "f1",
+                            Back = "b1"
+                        }
+                        ,new Card
+                        {
+                            CardId = 2,
+                            Front = "f2",
+                            Back = "b2"
+                        }
+                    },
+                DeckId = 1,
+                DeckTitle = "Dummy Deck",
+                IsShared = false,
+                ResourceId = 1,
+                ResourceTitle = "Dummy Resource",
+                Skip = 0,
+                Take = 2,
+                TotalCardCount = 20
+
+            };
+
+            _deckService
                 .Setup(d => d.ListCards(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>()))
-                .Returns( new CardListDto
-                {
+                .Returns(mockCardListDto)
+                .Verifiable() ;
 
-                });
+            _deckService
+                .Setup(ds => ds.SearchCardsInResource(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>()))
+                .Returns(mockCardListDto)
+                .Verifiable();
 
-
-
-            _deckService = deckService.Object;
-            _repositoryWrapper = repositoryWrapper.Object;
-            _codeRenderService = codeRenderService.Object;
         }
 
 
         [Fact]
-        public void Test1()
+        public void Index_Should_ReturnCardListDto()
         {
+            //Arrange
+            var _sut = new CardController(_deckService.Object, _repositoryWrapper.Object, _codeRenderService.Object);
 
+            //Act
+            var result = _sut.Index(1).ViewData.Model;
+
+            //Assert
+            Assert.IsType<CardListDto>(result);
+            _deckService.Verify(ds => ds.ListCards(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void SearchCardsInAllDecks_Should_ReturnCardListDto()
+        {
+            //Arrange
+            var _sut = new CardController(_deckService.Object, _repositoryWrapper.Object, _codeRenderService.Object);
+
+            //Act
+            var result = _sut.SearchCardsInAllDecks(1).ViewData.Model;
+
+            //Assert
+            Assert.IsType<CardListDto>(result);
+            _deckService.Verify(ds => ds.SearchCardsInResource(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>()), Times.Once);
         }
     }
 }
